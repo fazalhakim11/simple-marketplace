206638d451060aeb36700c35d91fd6d0367da469
Faza Alhakim <fazafal04@gmail.com>
2024-06-24 17:56:48 +0700

    1.5.8 edit orderHistory page layout #2

--- src/pages/orderHistory/components/Card/index.jsx
+++ src/pages/orderHistory/components/Card/index.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import useProductStores from "../../../../stores/productStores";
 
 const index = (props) => {
     const formatNumber = useProductStores((state)=> state.formatNumber)
+
+    const [seeDetail, SetSeeDetail] = useState(false)
+    const handleSeeDetail =()=>{
+      SetSeeDetail(!seeDetail)
+    }
   return (
     <div className="d-flex justify-content-between mb-3 border-bottom ">
         <div className=" d-flex mb-3">
             <div className="ms-3">
+              {seeDetail ? 
+              <div>
                 {Object.keys(props.product).filter(key => typeof props.product[key] === 'object').map(key=>(
                   <div className="d-flex mb-3" key={key}>
                     <div>
@@ -21,6 +29,21 @@ const index = (props) => {
                 <h5>Total Price: Rp {formatNumber(props.totalPrice)}</h5>
                 <p className="m-0">Payment: Rp {formatNumber(props.payment)}</p>
                 <p>Change: Rp {formatNumber(props.change)}</p>
+                <button className="p-0 bg-transparent text-primary fw-normal" onClick={handleSeeDetail}>Hide Detail</button>
+              </div>
+              :
+              <div className="d-flex mb-3">
+                <div>
+                  <img style={{width: "100px"}} className="rounded" src={props.product[0].img} alt={props.product[0].alt} />
+                </div>
+                <div className="ms-3"> 
+                  <h5>{props.product[0].title}</h5>
+                  <p className="m-0">Quantity: {props.product[0].quantity}</p>
+                  <p className="m-0">Price: {props.product[0].quantity} x Rp {formatNumber(props.product[0].price)} = Rp {formatNumber(props.product[0].quantity * props.product[0].price)}</p>
+                  <button className="p-0 bg-transparent text-primary fw-normal" onClick={handleSeeDetail}>See Detail</button>
+                </div>
+              </div>
+              }
             </div>
         </div>
         <p className="align-self-end ">{props.date}</p>
