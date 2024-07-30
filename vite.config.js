fada5e4d7d1cef7d1916239fc36c5049edd30607
Faza Alhakim <fazafal04@gmail.com>
2024-07-30 12:01:47 +0700

    Deploy #2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/simple-marketplace",
+  base: "/simple-marketplace/",
   plugins: [react()],
 })
